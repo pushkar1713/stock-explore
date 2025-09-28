17106fa9096d88c6be636d6b6ae08b52b717ca65
pushkar1713 <pushkar1713@gmail.com>
2025-09-28 11:21:27 +0530

    first commit : backend and frontend structures

---
+++ backend/src/app.ts
@@ -0,0 +1,30 @@
+import yahooFinance from "yahoo-finance2";
+
+const mostActive = await yahooFinance.screener({ scrIds: "most_actives" });
+const dailygainers = await yahooFinance.screener({
+  scrIds: "day_gainers",
+  count: 10,
+});
+const daily_loser = await yahooFinance.screener({
+  scrIds: "day_losers",
+  count: 10,
+});
+
+// const insights = await yahooFinance.insights("AAPL");
+// console.log(insights);
+
+const results = await yahooFinance.quoteSummary("^NDX");
+console.log(results);
+
+console.log("Most Active Stocks:");
+mostActive.quotes.forEach((stock: any) => {
+  // console.log(stock);
+  const volume = (stock.regularMarketVolume / 1e6).toFixed(2);
+  console.log(`${stock.symbol}: ${volume}M shares traded`);
+});
+
+dailygainers.quotes.forEach((stock: any) => {
+  // console.log(stock);
+  const volume = (stock.regularMarketVolume / 1e6).toFixed(2);
+  console.log(`${stock.symbol}: ${volume}M shares traded`);
+});
